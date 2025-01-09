c64d77ab7bf0c7f2ea47dd5b148319700c48c84c
DrDurka <dom3433219@gmail.com>
2025-01-09 17:48:01 +0300

    Add files via upload

---
+++ ExclusionList.gd
@@ -0,0 +1,18 @@
+extends Reference
+class_name ExclusionList
+
+static func getList():
+	return {
+		"arms":[
+		#NudePolice
+		"LatexStraitjacket",
+		],
+		"legs":[
+		#RabbitSpecies
+		"rabbitlegs",
+		"lopunnylegs"
+		],
+		"head":[],
+		"ears":[],
+	}
+
